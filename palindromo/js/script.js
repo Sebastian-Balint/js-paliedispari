b5ed99d771b27c1555b5ffbe5d300691845967dc
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-14 16:00:15 +0000

    add: innit palindromo

---
+++ palindromo/js/script.js
@@ -0,0 +1,8 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Su internet ci sono soluzioni con join e reverse, non potete usarle!
+
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
