38f22c81182ab68889a108a08ba6d9a5a808e273
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-14 16:27:27 +0000

    add: finished palindromo

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -6,3 +6,27 @@
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+//Creare funzione con 1 parola in ingresso:
+//SE la variabile parola invertita è uguale alla parola inserita dall'utente, allora ritorna true
+//ALTRIMENTI ritorna false
+
+function isPalindrome(word) {
+  let invertedWord = "";
+
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    //con ciclo for, passare ogni lettera della parola partendo dalla fine (length-1) e arrivando all'inizio (indice è >=0). Ciclo deve essere inverso, quindi i--.
+    let letter = userWord[i]; //creare variabile in cui concateno ogni lettera durante il ciclo, creando la parola invertita
+    invertedWord += letter;
+  }
+
+  if (word === invertedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let userWord = prompt("Inserisci una parola");
+
+console.log(isPalindrome(userWord));
